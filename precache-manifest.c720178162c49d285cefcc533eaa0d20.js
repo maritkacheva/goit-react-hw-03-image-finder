f46e1efb33049c781795a0b72bef4dedff6fab20
Maryna Tkachova <maritkacheva@hotmail.com>
2019-10-27 22:23:22 +0200

    Updates

---
+++ precache-manifest.c720178162c49d285cefcc533eaa0d20.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e8d8d08dddb058b706e181a91648f17",
+    "url": "/goit-react-hw-03-image-finder/index.html"
+  },
+  {
+    "revision": "db101e950bb6b41a7db5",
+    "url": "/goit-react-hw-03-image-finder/static/css/main.3d4d3b10.chunk.css"
+  },
+  {
+    "revision": "86f8d44398a96e6b619f",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.7d0e13ab.chunk.js"
+  },
+  {
+    "revision": "db101e950bb6b41a7db5",
+    "url": "/goit-react-hw-03-image-finder/static/js/main.1cd78314.chunk.js"
+  },
+  {
+    "revision": "5c0ffd13c8101a377548",
+    "url": "/goit-react-hw-03-image-finder/static/js/runtime~main.5e2bcc82.js"
+  }
+]);
